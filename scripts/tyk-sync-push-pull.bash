89d175ef2ee0b344ea5faf5b4034cef8ee34763c
Sedky <sedkyaboushamalah@gmail.com>
2020-07-02 15:40:27 -0400

    tyk-sync docker example

--- scripts/tyk-sync-push-pull.bash
+++ scripts/tyk-sync-push-pull.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Edit these
+local_dashboard=http://host.docker.internal:3000
+local_dashboard_api_key=d98866b4a83b488954e05151de88da2f
+remote_dashboard=http://remote-dash-host:3000/
+remote_dashboard_api_key=my-key
+
 ### create tmp directory
 mkdir tmp
 cd tmp
@@ -8,15 +14,16 @@ cd tmp
  docker run --rm \
  --mount type=bind,source="$(pwd)",target=/opt/tyk-sync/tmp \
  tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740 \
- dump -d="http://host.docker.internal:3000" -s="d98866b4a83b488954e05151de88da2f" -t="./tmp"
+ dump -d="$local_dashboard" -s="$local_dashboard_api_key" -t="./tmp"
 
 
 ### push to new dashboard
 docker run --rm \
  --mount type=bind,source="$(pwd)",target=/opt/tyk-sync/tmp \
- tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740 sync -d="http://35.202.210.74:3000/" -s="0215a8fb37d2434e77f81e1f184a7292" -p "tmp" -o="5efe192917f51f000155815d"
+ tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740 sync -d="$remote_dashboard" -s="$remote_dashboard_api_key" -p "tmp" -o="5efe192917f51f000155815d"
 
 ## cleanup tmp stuff
 rm *
+rm .tyk.json
 cd ..
 rmdir tmp
