d0b6765f15e75dcf0cbd132afb7f5d9a9676a814
Sedky <sedkyaboushamalah@gmail.com>
2020-07-13 14:44:48 -0400

    add version to the tyk-sync

--- scripts/tyk-sync-push-pull.bash
+++ scripts/tyk-sync-push-pull.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 ## Edit these
-local_dashboard=https://admin.cloud.tyk.io/
-local_dashboard_api_key=9581c604ba864b3878fe1f6b0d1643f8
-remote_dashboard=http://34.71.229.10:3000
-remote_dashboard_api_key=886aa81f2e0541d76096342fb1d3e38a
-remote_org_id=5f01f092b8630e000183076f
+local_dashboard=https://admin.cloud.tyk.io
+local_dashboard_api_key=12345
+remote_dashboard=http://host.docker.internal:3000
+remote_dashboard_api_key=7b5d4aa3456346d448304e1e586e12a8
+remote_org_id=5e9d9544a1dcd60001d0ed20
+
+## don't need to edit probably
+# tyk_sync_version=tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740
+tyk_sync_version=sedkis/tyk-sync:master-2
 
 ### create tmp directory
 mkdir tmp
@@ -14,7 +18,7 @@ cd tmp
 ### download stuff onto local file system
  docker run --rm \
  --mount type=bind,source="$(pwd)",target=/opt/tyk-sync/tmp \
- tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740 \
+ $tyk_sync_version \
  dump \
  -d="$local_dashboard" \
  -s="$local_dashboard_api_key" \
@@ -24,7 +28,7 @@ cd tmp
 ### push to new dashboard
 docker run --rm \
  --mount type=bind,source="$(pwd)",target=/opt/tyk-sync/tmp \
- tykio/tyk-sync:v1.1.0-27-gbf4dd2f-3-g04f7740 \
+$tyk_sync_version \
  sync \
  -d="$remote_dashboard" \
  -s="$remote_dashboard_api_key" \
